723ddc1a32b10205cd4ea62112fcd17f8af30e18
Adebayo <mradeybee@gmail.com>
2015-12-07 20:40:50 +0100

    controller methods added

--- app/controllers/v1/items_controller.rb
+++ app/controllers/v1/items_controller.rb
@@ -1,2 +1,38 @@
-class ItemsController < ApplicationController
+module V1
+  class ItemsController < ApplicationController
+    def index
+      @item = Item.all
+      render json: @item
+    end
+
+    def show
+      @item = Item.find(params[:id])
+      render json: @item
+    end
+
+    def new
+      @item = Item.new(item_params) if item_params
+      render json: @item
+    end
+
+    def edit
+    end
+
+    def create
+      @item = Item.create(item_params) if item_params
+      render json: @item
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      @item.update(item_params) if item_params
+      render json: @item
+    end
+
+    private
+
+    def item_params
+      params.permit(:bucketlist_id, :name, :details, :done)
+    end
+  end
 end
