42496eccb3eb14e5cc611e7d720cdf364f9e0634
Adebayo <mradeybee@gmail.com>
2015-12-11 23:46:05 +0100

    route corrction made

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1 do
     post "auth/login", to: "auth#login", as: "login"
-    post "auth/logout", to: "auth#logout", as: "logout"
+    get "auth/logout", to: "auth#logout", as: "logout"
     resources :users
     resources :bucketlists do
       resources :items
